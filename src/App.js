f2706912ded91cf1b255ffdd473d9e8efd5f00e2
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-03-07 03:57:20 -0600

    added categoies

--- src/App.js
+++ src/App.js
@@ -23,12 +23,19 @@ class App extends Component {
         <aside className="sideMenu">
           <ul>
             <li><button>Menu</button></li>
-            <li><button>Healths</button></li>
+            <li><button>Health</button></li>
             <li><button>Games</button></li>
             <li><button onClick={this.openTab('videos')}>Videos</button></li>
             <li><button>Stories</button></li>
-            <li><button>Maths</button></li>
+            <li><button>Math</button></li>
             <li><button>Tools</button></li>
+            <li><button>Astronomy</button></li>
+            <li><button>Quotes</button></li>
+            <li><button>Instrumentation</button></li>
+            <li><button>Money</button></li>
+            <li><button>Spanish</button></li>
+            <li><button>Guitar</button></li>
+            <li><button>Media lists</button></li>
             <li><button onClick={this.openTab('church')}>Church</button></li>
             <li><button onClick={this.openTab('links')}>Links</button></li>
           </ul>
--- src/components/videos.js
+++ src/components/videos.js
@@ -3,7 +3,15 @@ import React from 'react';
 class Videos extends React.Component {
   render () {
     return (
-      <div>videos</div>
+      <div>
+      <h3>Movie Trailors</h3>
+      <h3>Favorite Music</h3>
+      <h3>Programmig</h3>
+      <h3>Church</h3>
+      <h3>How To</h3>
+      <h3>Funny</h3>
+      <h3>Other Favorites</h3>
+      </div>
     );
   }
 }
