56158fbe3df7dd64f71d45984bd583b1d9ef5957
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-04-02 17:03:54 -0500

    Added content to movies and menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,6 +4,17 @@ class Menu extends React.Component {
   render () {
     return (
       <div>
+      <div id="menuTier1"></div>
+        <button class="tier1">Drinks</button>
+        <button class="tier1">Apetizers</button>
+        <button class="tier1">Breakfast</button>
+        <button class="tier1">Entree'</button>
+        <button class="tier1">Desert</button>
+        <button class="tier1">Soup</button>
+        <button class="tier1">Sauces</button>
+        <button class="tier1">Ingredients</button>
+      <div id="menuTier2"></div>
+      <div id="menuTier3"></div>
         <ul>
           <li>Keto Icecream</li>
         </ul>
