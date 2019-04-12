1c3ca52bd65cf96a319b65323f4f5ae86d0c8b3f
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-04-12 00:26:06 -0500

    tier 2 for Bom

--- src/components/church.js
+++ src/components/church.js
@@ -1,37 +1,79 @@
 import React from 'react';
 
 class Church extends React.Component {
+  state = {
+    tier1: undefined,
+    tier2: undefined,
+  }
+  pickTier1 = (item) => {
+    return () => {
+      this.setState({tier1:item})
+    }
+  }
+  pickTier2 = (item) => {
+    return() => {
+      this.setState({tier2:item})
+    }
+  }
+
   render () {
+    const bomItems = [
+      {name: '1 Nephi', slug: 'nephi1'},
+      {name: '2 Nephi', slug: 'nephi2'},
+      {name: 'Jacob', slug: 'jacob'},
+      {name: 'Enos', slug: 'enos'},
+      {name: 'Jarom', slug: 'jarom'},
+      {name: 'Omni', slug: 'omni'},
+      {name: 'Words of Mormon', slug: 'wom'},
+      {name: 'Mosiah', slug: 'mosiah'},
+      {name: 'Alma', slug: 'alma'},
+      {name: 'Helaman', slug: 'helaman'},
+      {name: '3 Nephi', slug: 'nephi3'},
+      {name: '4 Nephi', slug: 'nephi4'},
+      {name: 'Mormon', slug: 'mormon'},
+      {name: 'Ether', slug: 'ether'},
+      {name: 'Moroni', slug: 'moroni'},
+    ]
+    const churchItems = [
+      {name: 'BOM', slug: 'bom', tier2: bomItems},
+      {name: 'D&C', slug: 'dc'},
+      {name: 'POGP', slug: 'pogp'},
+      {name: 'OT', slug: 'ot'},
+      {name: 'NT', slug: 'nt'},
+      {name: 'Symbolism', slug: 'symbolism'},
+      {name: 'Texts', slug: 'texts'},
+      {name: 'Prophecy', slug: 'prophecy'},
+      {name: 'Subjects', slug: 'subjects'},
+    ]
+    const renderChurchItems = churchItems.map(item => {
+      return (
+        <button
+          key={item.slug}
+          onClick={this.pickTier1(item.slug)}
+          className={this.state.tier1 === item.slug ? "tier1 active" : "tier1"}
+          >{item.name}</button>
+      )
+    })
+    const activeTier1Item = this.state.tier1 && churchItems.filter(item => {
+      return this.state.tier1 === item.slug;
+    })
+    const tier2 = activeTier1Item && (activeTier1Item[0].tier2 || []);
+    const renderTier2 = tier2 && tier2.map(item => {
+      return (
+        <button
+          key={item.slug}
+          onClick={this.pickTier2(item.slug)}
+          className={this.state.tier2 === item.slug ? "tier2 active" : "tier2"}
+          >{item.name}</button>
+      )
+    })
     return (
-      <div>
-          <div id="churchTeir1">
-            <button class="tier1">Apologetics</button>
-            <button class="tier1">BOM</button>
-            <button class="tier1">NT</button>
-            <button class="tier1">OT</button>
-            <button class="tier1">D&C</button>
-            <button class="tier1">POGP</button>
-            <button class="tier1">Sacred Geometry</button>
-            <button class="tier1">Symbolism</button>
-            <button class="tier1">Other Texts</button>
-            <button class="tier1">Prophecy</button>
-            <button class="tier1">Faith/Knowledge</button>
-            <button class="tier1">Priesthood</button>
-            <button class="tier1">Law of Consecration</button>
-            <button class="tier1">Law of God</button>
-            <button class="tier1">Law of Sacrifice</button>
-            <button class="tier1">Law of Obedience</button>
-            <button class="tier1">Creation</button>
-            <button class="tier1">Fall</button>
-            <button class="tier1">Pre-mortal Existence</button>
-            <button class="tier1">Spirit World</button>
-            <button class="tier1">Clear All</button>
-            <hr />
-          </div>
-          <div id="churchTeir2"></div>
-          <div id="churchTeir3"></div>
-      </div>
-    );
+      <ul>
+        {renderChurchItems}
+        <hr />
+        {renderTier2}
+      </ul>
+    )
   }
 }
 
