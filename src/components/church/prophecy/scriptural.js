66fe98b22e372e2306ac730f5f9c6173646d558e
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-04-25 00:45:53 -0500

    change text format prophecy

--- src/components/church/prophecy/scriptural.js
+++ src/components/church/prophecy/scriptural.js
@@ -5,179 +5,151 @@ class Scriptural extends React.Component {
     return (
       <div>
       <h3>Doctrine and Covenants</h3>
-        <ol>
-        1-The Lord and Satan will both have power <br />
-        over their dominions.  This will happen<br />
-        after or as peace is taken from the Earth(35,36)<br /><br />
-        5-A desolating scourge(sickness) will not stop<br />
-        until the coming of the savior(18,19)<br /><br />
-        45-Remnant will remain scattered until the time<br />
-        of the Gentiles is fulfilled.  There will be wars<br />
-        and rumors of wars.The earth will be in commotion.<br />
-        Men's hearts will fail them.  They will claim<br />
-        delays His comming.  The love of men will wax<br />
-        cold. Evil will be everywhere.  The gospel in<br />
-        its fulness will be brought forth at the<br />
-        beginning of the time of the gentiles.  The <br />
-        desolating scourge will happen and cover the<br />
-        land.  The saints will stand in holy places<br />
-        of protection.  There will be earthquakes and<br />
-        war.The sign of the comming of the son of man,<br />
-        blood, fire, and vapor of smoke.  There will<br />
-        be wonders in heaven and in the earth.  The<br />
-        sun will be darkened and the moon turned to<br />
-        blood.  Stars will fall from heaven. The <br />
-        remnant will be gathered to Jerusalen and see<br />
-        Christ return in a cloud of glory with all His<br />
-        angels.  Before the arm of the Lord falls, an<br />
-        angel will sound his trump, and the saints<br />
-        to meet Christ in the sky.After that, the <br />
-        arm of the Lord falls on the nations.  Then,<br />
-        Christ will step on the Mount of Olives<br />
-        breaking it in half.  The earth and<br />
-        heavens will shake and the Earth will real<br />
-        to and fro.  The Lords voice shall be heard<br />
-        across the entir Earth.  The wickid will be<br />
-        destroyed.  The Jews wil see Christ and accept<br />
-        Him.  There will then be a resurection of the<br />
-        heathen nations and Satan is bound.(24-57)<br />
-        The New Jerusalem in Missouri, will be a place<br />
-        of refuge and Safety for the saints.  The wickid<br />
-        will not come to Jerusalem.  The terror of the<br />
-        the Lord will be there.  It will be called Zion.<br />
-        If any man doesn't want to fight in the chaos, he<br />
-        will have to come to Zion.  It will be the only<br />
-        people not at war with its neighbor.(63-75)<br /><br />
-        49-The heaven and Earth will be shaken.  At the<br />
-        sounding of the trump, mountains and Valleys will<br />
-        be made smooth.Before the Second Comming, Jacob<br />
-        shall flourish in the desert, and the Lamanites<br />
-        will blossom as a rose.  Zion will flourish<br />
-        upon the hills and rejoice in the place they<br />
-        have been gathered.(22-25).<br /><br />
-        58-The feast of fat things will first be to<br />
-        the rich and learned.  After that comes the<br />
-        day of power, when the poor, lame, blind<br />
-        and deaf will partake of the supper of <br />
-        the Lord, prepared for the Great Day to Come(5-11)<br /><br />
-        61-In the Last Days the waters are cursed.<br />
-        The day will come when no one is safe on<br />
-        the waters.  Only the upright in heart will<br />
-        be able to come to Zion by water.(14-19)<br /><br />
-        77-The seven seals represent the 7<br />
-        thousands years of Earth's temporal history.<br />
-        The 4 angels of revelation 4 are given power<br />
-        over the four quarters of the Earth.  They have<br />
-        the everlasting gospel and the power over life<br />
-        and death.  The angel coming from the East has<br />
-        the seal of God over the tribes of Israel.  He<br />
-        stops the destroying angels from hurting the <br />
-        earth until the saints can be sealed in the<br />
-        forehead.  These things will happen durring<br />
-        the sixth seal or 6 thousand years.  The 144000<br />
-        are sealed High Priests after the Holy Order<br />
-        of God.  They are ordained out of every nation<br />
-        by the 4 angels to bring people to the church of<br />
-        the Firstborn.  In Revelation 8, at the beginning<br />
-        of the 7th seal, God will sanctify the Earth and<br />
-        redeem all things put in His power.  The trumpets<br />
-        are the 7 angels signalling the preparing and <br />
-        finishing of His work in the beginning of the <br />
-        seventh thousand years.  The preparing of the<br />
-        way before the time of His comming.The seventh<br />
-        will be opened and Chapter 9 of Revelation will<br />
-        be fulfilled before His comming.  John is the<br />
-        spirit of Elias that will gather the tribes.<br />
-        The 2 witnesses in Revelation 11 are two prophets<br />
-        raised up to the Jewish nation who will prophesy<br />
-        to the Jews in Jerusalem(6-15)<br /><br />
-        84-Temple lot of Zion will be built in Western<br />
-        Missouri(1-5).  The sons of Moses and <br />
-        Aaron(bearers of Aaronic and Melchizedek<br />
-        priesthoods)will make a sacrifice in the <br />
-        temple(31-34) Plagues will go forth on the cities<br />
-        and nations that reject the word of the Lord and will<br />
-        not stop until His comming.  Eventually all will<br />
-        know the Lord.  The song of Zion is Given.<br /><br />
-        85-One mighty and strong will be sent holding the<br />
-        scepter of power, clothed with light, who will be<br />
-        full of truth who will set in order the house of God.<br />
-        He will arrange the lots of inheritance according<br />
-        to the Book of Life and geneology.  A man called<br />
-        and appointed by God will try to steady the arc land<br />
-        fall by the shaft of death like a tree struck by <br />
-        lightening.  Those not found in the Book of<br />
-        rememberence will not recieve an inheritance.(7-12)<br /><br />
-        87-Civil war predicted. Great brittain calls on other<br />
-        nations and war will be poured out on all nations.<br />
-        After many days, slaves will revolt against masters<br />
-        and they will be marshalled for war.  The remnant will<br />
-        also marshall themselves and will vex the Gentiles.<br />
-        There will be bloodshed, famine, earthquakes, thunder<br />
-        and lightening.  The inhabitants of the earth will<br />
-        suffer until the consumption decreed has made an end<br />
-        of all nations.  Stand in holy places until the day<br />
-        of the Lord comes(3-8)<br /><br />
-        88-prepare the saints for the hour of judgement.<br />
-        The earth will real to and fro. The sun will be<br />
-        darkened.  The moon will be bathed in blood, and<br />
-        the stars will fall.  After your testimony comes<br />
-        the testimony of earthquakes, and storms, and <br />
-        the sea heaving itself beyond its bounds.  Men's<br />
-        hearts will fail them.  Angels will fly through<br />
-        heaven sounding the trump saying prepare ye, the<br />
-        bridegroom cometh; go ye out to meet Him.  This<br />
-        will imediately be followed by a great sign in<br />
-        heaven and the whole world will see it together.<br />
-        Another trump will sound and an angel will<br />
-        announce how the great and abominable church is<br />
-        bound and ready to be burned.  He will sound his<br />
-        trump, and there will be silence in heaven for<br />
-        the space of half an hour.  Imediately after<br />
-        the half hour, the curtain of heaven will be<br />
-        unfolded and the face of the Lord will be unveiled.<br />
-        The saints will be caught up, and the saints will <br />
-        be resurected and caught up into heaven.This will<br />
-        all happen to the sound of the angel's trump.<br />
-        Another trump will sound for the resurection of<br />
-        the righteous spirits who recieved the gospel<br />
-        in the spirit world.  Another trump will sound<br />
-        and all the spirits of those under condemnation<br />
-        will remain dead until the end of the thousand<br />
-        years.The fifth trump is the angel who commiteth<br />
-        the everlasting gospel, and he will say:Fear God<br />
-        and give glory to God.  The sixth trump will sound<br />
-        and it will be announced that the Church of the<br />
-        Devil has fallen.  The seventh trump will sound<br />
-        and the angel will announce that the lamb has<br />
-        overcome.  The angels will be crowned with glory.<br />
-        They will recieve their inheritance and be made<br />
-        equal with Christ.  7 Trumpets will announce<br />
-        the acts of the 7 thousand years and the binding<br />
-        of Satan for 1000 years.(84-110)<br /><br />
-        90-The gospel will go first to the Gentiles<br />
-        and then to the Jews.  Only after that will the<br />
-        Lord be revealed in power to the convincing of all<br />
-        nations.  Then every man will hear the gospel in<br />
-        his own language.(9-11)<br /><br />
+        <p>
+        1-The Lord and Satan will both have power over their dominions. This
+        will happen after or as peace is taken from the Earth(35,36)
+        </p>
+        <p>
+        5-A desolating scourge(sickness) will not stop until the coming of the
+        savior.(18,19)
+        </p>
+        <p>
+        45-Remnant will remain scattered until the time of the Gentiles is
+        fulfilled. There will be wars and rumors of wars.The earth will be in
+        commotion. Men's hearts will fail them. They will claim He delays His
+        comming. The love of men will wax cold. Evil will be everywhere. The
+        gospel in its fulness will be brought forth at the beginning of the time
+        of the gentiles. The desolating scourge will happen and cover the land.
+        The saints will stand in holy places of protection. There will be
+        earthquakes and war. The sign of the comming of the son of man, blood,
+        fire, and vapor of smoke. There will be wonders in heaven and in the
+        earth. The sun will be darkened and the moon turned to blood. Stars will
+        fall from heaven. The remnant will be gathered to Jerusalen and see
+        Christ return in a cloud of glory with all His angels. Before the arm of
+        the Lord falls, an angel will sound his trump, and the saints to meet
+        Christ in the sky.After that, the arm of the Lord falls on the nations.
+        Then, Christ will step on the Mount of Olives breaking it in half. The
+        earth and heavens will shake and the Earth will real to and fro.  The
+        Lord's voice shall be heard across the entir Earth. The wickid will be
+        destroyed. The Jews wil see Christ and accept Him. There will then be a
+        resurection of the heathen nations and Satan is bound.(24-57)The New
+        Jerusalem in Missouri, will be a place of refuge and Safety for the
+        saints. The wickid will not come to Jerusalem. The terror of the Lord
+        will be there. It will be called Zion.  If any man doesn't want to
+        fight in the chaos, he will have to come to Zion. It will be the only
+        people not at war with its neighbor.(63-75)
+        </p>
+        <p>
+        49-The heaven and Earth will be shaken. At the sounding of the trump,
+        mountains and Valleys will be made smooth. Before the Second Comming,
+        Jacob shall flourish in the desert, and the Lamanites will blossom as a
+        rose.  Zion will flourish upon the hills and rejoice in the place they
+        have been gathered.(22-25).
+        </p>
+        <p>
+        58-The feast of fat things will first be to the rich and learned. After
+        that comes the day of power, when the poor, lame, blind and deaf will
+        partake of the supper of the Lord, prepared for the Great Day to
+        Come(5-11)
+        </p>
+        <p>
+        61-In the Last Days the waters are cursed. The day will come when no one
+        is safe on the waters. Only the upright in heart will be able to come to
+        Zion by water.(14-19)
+        </p>
+        <p>
+        77-The seven seals represent the 7 thousands years of Earth's temporal
+        history. The 4 angels of revelation 4 are given power over the four
+        quarters of the Earth. They have the everlasting gospel and the power
+        over life and death. The angel coming from the East has the seal of God
+        over the tribes of Israel. He stops the destroying angels from hurting
+        the earth until the saints can be sealed in the forehead. These things
+        will happen durring the sixth seal or 6 thousand years. The 144000 are
+        sealed High Priests after the Holy Order of God. They are ordained out
+        of every nation by the 4 angels to bring people to the church of the
+        Firstborn. In Revelation 8, at the beginning of the 7th seal, God will
+        sanctify the Earth and redeem all things put in His power. The trumpets
+        are the 7 angels signalling the preparing and finishing of His work in
+        the beginning of the seventh thousand years. The preparing of the way
+        before the time of His comming.The seventh will be opened and Chapter 9
+        of Revelation will be fulfilled before His comming. John is the spirit
+        of Elias that will gather the tribes. The 2 witnesses in Revelation 11
+        are two prophets raised up to the Jewish nation who will prophesy to the
+        Jews in Jerusalem(6-15)
+        </p>
+        <p>
+        84-Temple lot of Zion will be built in Western Missouri(1-5). The sons
+        of Moses and Aaron(bearers of Aaronic and Melchizedek priesthoods)will
+        make a sacrifice in the temple(31-34) Plagues will go forth on the
+        cities and nations that reject the word of the Lord and will not stop
+        until His comming. Eventually all will know the Lord. The song of Zion
+        is Given.
+        </p>
+        <p>
+        85-One mighty and strong will be sent holding the scepter of power,
+        clothed with light, who will be full of truth who will set in order the
+        house of God. He will arrange the lots of inheritance according to the
+        Book of Life and geneology.  A man called and appointed by God will try
+        to steady the arc land fall by the shaft of death like a tree struck by
+        lightening. Those not found in the Book of rememberence will not recieve
+        an inheritance.(7-12)
+        </p>
+        <p>
+        87-Civil war predicted. Great brittain calls on other nations and war
+        will be poured out on all nations. After many days, slaves will revolt
+        against masters and they will be marshalled for war. The remnant will
+        also marshall themselves and will vex the Gentiles. There will be
+        bloodshed, famine, earthquakes, thunder and lightening. The inhabitants
+        of the earth will suffer until the consumption decreed has made an end
+        of all nations. Stand in holy places until the day of the Lord
+        comes(3-8)
+        </p>
+        <p>
+        88-prepare the saints for the hour of judgement. The earth will real to
+        and fro. The sun will be darkened. The moon will be bathed in blood, and
+        the stars will fall. After your testimony comes the testimony of
+        earthquakes, and storms, and the sea heaving itself beyond its bounds.
+        Men's hearts will fail them. Angels will fly through heaven sounding
+        the trump saying prepare ye, the bridegroom cometh; go ye out to meet
+        Him. This will imediately be followed by a great sign in heaven and the
+        whole world will see it together. Another trump will sound and an angel
+        will announce how the great and abominable church is bound and ready to
+        be burned. He will sound his trump, and there will be silence in heaven
+        for the space of half an hour. Imediately after the half hour, the
+        curtain of heaven will be unfolded and the face of the Lord will be
+        unveiled. The saints will be caught up, and the saints will be
+        resurected and caught up into heaven.This will all happen to the sound
+        of the angel's trump. Another trump will sound for the resurection of
+        the righteous spirits who recieved the gospel in the spirit world.
+        Another trump will sound and all the spirits of those under condemnation
+        will remain dead until the end of the thousand years.The fifth trump is
+        the angel who commiteth the everlasting gospel, and he will say:Fear God
+        and give glory to God. The sixth trump will sound and it will be
+        announced that the Church of the Devil has fallen. The seventh trump
+        will sound and the angel will announce that the lamb has overcome. The
+        angels will be crowned with glory. They will recieve their inheritance
+        and be made equal with Christ. 7 Trumpets will announce the acts of the
+        7 thousand years and the binding of Satan for 1000 years.(84-110)
+        </p>
+        <p>
+        90-The gospel will go first to the Gentiles and then to the Jews. Only
+        after that will the Lord be revealed in power to the convincing of all
+        nations. Then every man will hear the gospel in his own language.(9-11)
+        </p>
+        <p>
         97-
-        101,103,105,112,113,116,133<br />
-        </ol>
+        101,103,105,112,113,116,133
+        </p>
       <h3>Book of Mormon</h3>
-      <ol>
         1Ne 8,10-15,19-22<br />
         2Ne 3,6-9,12-24,26-30<br />
         Jacob 5,6<br />
         3 Ne 16,20-22,24,25<br />
         Ether 4,8,13<br />
-      </ol>
       <h3>Pearl of Great Price</h3>
-      <ol>
         Moses 7<br />
         JSM 1<br />
-      </ol>
       <h3>New Testament</h3>
-      <ol>
         Matt 24,25<br />
         Romans 11<br />
         2 Thes 1,2<br />
@@ -186,9 +158,7 @@ class Scriptural extends React.Component {
         2 Peter 3<br />
         Jude 1<br />
         Rev 6-11,13-14,16-19<br />
-      </ol>
       <h3>Old Testament</h3>
-      <ol>
         Deut 30,33<br />
         Isaiah 1,18,21,24,25,27,30-35,43,44,52,59-63,66<br />
         Jeremiah 3,23,25,30-33,50<br />
@@ -204,7 +174,6 @@ class Scriptural extends React.Component {
         Zachariah 2,3,6,8,10,12-14<br />
         Joel 2-3<br />
         Mal 3-4<br />
-      </ol>
       <br /><br /><br /><br /><br />
       </div>
     );
