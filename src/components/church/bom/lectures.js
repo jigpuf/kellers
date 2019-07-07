5565df93c35d9b507fb52a3260f07d23d8f91b7e
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-07-06 21:54:34 -0500

    added lectures on bom

--- src/components/church/bom/lectures.js
+++ src/components/church/bom/lectures.js
@@ -4,10 +4,69 @@ class Lectures extends React.Component {
   render () {
     return (
       <div>
-        <li>BYU Roundtable</li>
-        <li>Hugh Nibley</li>
-        <li>Hyrum Andrus</li>
-        <li>Interpreter Roundtable</li>
+        <h3>Hugh Nibley</h3>
+          <div class='twoColumn'>
+          <a href="https://www.youtube.com/watch?v=XraGjlIcjrs&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw" target ='_blank'
+          >1-Like No other Book</a><br />
+          <a href="https://www.youtube.com/watch?v=z6kQAB-fmPk&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=2" target ='_blank'
+          >2-Nephi's Heritage</a><br />
+          <a href="https://www.youtube.com/watch?v=rSV4hKlo08I&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=3" target ='_blank'
+          >3-Geopolitics</a><br />
+          <a href="https://www.youtube.com/watch?v=G2Ok7MsoJfA&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=4" target ='_blank'
+          >4-600 BC</a><br />
+          <a href="https://www.youtube.com/watch?v=k6PSfvbDgMk&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=5" target ='_blank'
+          >5-Lehi's Contemporaries</a><br />
+          <a href="https://www.youtube.com/watch?v=zaLCrgkLUI0&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=6" target ='_blank'
+          >6-1Ne 1</a><br />
+          <a href="https://www.youtube.com/watch?v=qWOUjyeiTH8&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=7" target ='_blank'
+          >7-1Ne and Jeremiah</a><br />
+          <a href="https://www.youtube.com/watch?v=PCXVp4-mVM8&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=8" target ='_blank'
+          >8-Escape from Doom</a><br />
+          <a href="https://www.youtube.com/watch?v=O_LOL8-cN3o&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=9" target ='_blank'
+          >9-1Ne 1-3,15</a><br />
+          <a href="https://www.youtube.com/watch?v=muTEo3_ZPkE&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=10" target ='_blank'
+          >10-Dead Sea Scrolls</a><br />
+          <a href="https://www.youtube.com/watch?v=Ast6DcrtEy8&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=11" target ='_blank'
+          >11-1Ne 4-7 Scripture and Family</a><br />
+          <a href="https://www.youtube.com/watch?v=7GXGQLuKaao&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=12" target ='_blank'
+          >12-1Ne 8-11 Tree of Life</a><br />
+          <a href="https://www.youtube.com/watch?v=nzU76NqA0YM&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=13" target ='_blank'
+          >13-1Ne 12-14 Nephi's Vision</a><br />
+          <a href="https://www.youtube.com/watch?v=ZR7k9KG2GSk&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=14" target ='_blank'
+          >14-1Ne 15-16</a><br />
+          <a href="https://www.youtube.com/watch?v=aTCl1Xwkarw&list=PLfv_F_JnfozRI-BetijBCBaXZR2XF9Nyw&index=15" target ='_blank'
+          >15-1Ne 17-22 Toward the promised Land</a><br />
+          <a href="https://www.youtube.com/watch?v=K-LynsaL0WA" target ='_blank'
+          >16-2 Nephi 1-4 Atonement</a><br />
+          <a href="https://www.youtube.com/watch?v=4MAZjKrRIaw" target ='_blank'
+          >17-2 Nephi 2 The Law and The Atonement</a><br />
+          <a href="https://www.youtube.com/watch?v=m2vQtYRpkZ4" target ='_blank'
+          >18-2 Nephi 3-8</a><br />
+          <a href="https://www.youtube.com/watch?v=ka_hTcuJH4A" target ='_blank'
+          >19-2 Nephi 9 The Atonement & Judgment</a><br />
+          <a href="https://www.youtube.com/watch?v=0lwL8BG86y0" target ='_blank'
+          >20-2 Nephi 25 The Jews and Jerusalem</a><br />
+          <a href="https://www.youtube.com/watch?v=XXmRldzMEAc&t=1685s" target ='_blank'
+          >21-2 Nephi 25-28 Nephi's Prophecy of our Times</a><br />
+          <a href="https://www.youtube.com/watch?v=PIKX2biJap4&t=22s" target ='_blank'
+          >22-2 Nephi 29-31 Scripture and Cannon </a><br />
+          <a href="https://www.youtube.com/watch?v=A5Bx-fHBuQE" target ='_blank'
+          >23-2 Nephi 32--33; Jacob 1--2</a><br />
+          <a href="https://www.youtube.com/watch?v=cdGw9G2XJcE" target ='_blank'
+          >24-Jacob 3-4 Filthiness and the Atonement</a><br />
+          <a href="https://www.youtube.com/watch?v=2BOJTtx_udw" target ='_blank'
+          >25-Jacob 5--7; Enos</a><br />
+          <a href="https://www.youtube.com/watch?v=At2NgQghR8w" target ='_blank'
+          >26-Enos, Jarom, Omni</a><br />
+          <a href="https://www.youtube.com/watch?v=7BP3I8PFXW8" target ='_blank'
+          >27-Omni, Words of Mormon, Mosiah 1</a><br />
+          <a href="https://www.youtube.com/watch?v=yK4g2skg--Q" target ='_blank'
+          >28-Mosiah 1--2 King Benjamin's Speech</a><br />
+          <a href="https://www.youtube.com/watch?v=5ZN_QBNx6ps" target ='_blank'
+          >29-Mosiah 3--5 King Benjamin's Speech</a><br />
+          <a href="https://drive.google.com/drive/folders/0B4o7q1habMugMDlJMTNycFFNVms" target ='_blank'
+          >Hugh Nibley Lecture Streaming</a><br />
+          </div><hr/>
       </div>
     );
   }
