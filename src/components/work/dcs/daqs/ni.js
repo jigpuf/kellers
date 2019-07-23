2a270fc334b73abccb0b1e7cd1cc0e0a4fffef07
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-07-23 00:55:06 -0500

    Added module swap proceedure

--- src/components/work/dcs/daqs/ni.js
+++ src/components/work/dcs/daqs/ni.js
@@ -11,6 +11,22 @@ class Ni extends React.Component {
       <li>Configure and checkout Allen Bradley hardware(3)</li>
       <li>NI Platform choice making and configuration(3)</li>
       <li>Creating Build list and ordering Hardware</li>
+      <hr />
+      <h4>Module Swap Proceedure</h4>
+      <li>Tell engineering to bring down server</li>
+      <li>Shut Down PXI top to bottom</li>
+      <li>Shut Down SCXI Top to Bottom</li>
+      <li>Get New Module and scan to Slot</li>
+      Warpdrive - Warehouse Execution - 3Line Menu - Move Anything - scan module
+      bar code, scan slot bar code
+      <li>Get Old Module and scan to Self</li>
+      Warpdrive - Warehouse Execution - 3Line Menu -Move Anything - scan module
+      bar code, scan badge bar code
+      <li>Turn on PXIs Top to Bottom</li>
+      <li>Turn on SCXI Top to Bottom</li>
+      <li>Turn on Server</li>
+      <li>Tell Engineering it is comming on</li>
+      <li>Take old module to metrology</li>
       </div>
     );
   }
