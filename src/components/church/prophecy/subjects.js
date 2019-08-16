17597ae500c016490962d468d2585727c662aa77
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-08-15 22:13:18 -0500

    added parts ordering

--- src/components/church/prophecy/subjects.js
+++ src/components/church/prophecy/subjects.js
@@ -46,6 +46,26 @@ class Subjects extends React.Component {
           </tr>
         </table>
         Hyrum Andrus Timeline:<br />
+        1) Warfare against Zion<br />
+        1 Ne 14,22; 2Ne 18,20,30; 3Ne 20,21;Joel 2; Isaiah 4; Malachi 3; Rev 13; Dan 7<br />
+        JST p157,70-71; Rev 13<br />
+        1b1)  Rising of the 2 beasts(America(Babylon),Little Horn(Russia)(asyrian))<br />
+        1a) Great Division<br />
+        1b) Tribulation of Zion starting in America<br />
+        Russia attacks England then against America<br />
+        1c1)Adam-ondi-Ahman and arrival of Christ<br />
+        1b1a) Pouring out of wrath on Mother of Harlets<br />
+        1b2) Cleansing of America<br />
+        1c) Establishment of Zion system<br />
+        Safety in Zion, Jerusalem, The remnant(indians)<br />
+        1d) Endowment of Power<br />
+        1e) The great gathering commences<br />
+        <br />
+        2) Warfare against Jerusalem<br />
+        Joel 3;1Ne 14,22,2Ne 6, 3ne 16,20,21; Ezekiel 38,39<br />
+        3) Apearance to the world in Glory<br />
+        <br />
+        <br />
         Cleon Skousen Timeline:<br />
         Bruce R Mckonkie Timeline:<br />
         Joseph Fielding Smith Timeline:<br />
--- src/components/work/parts/numbers.js
+++ src/components/work/parts/numbers.js
@@ -4,221 +4,156 @@ class Numbers extends React.Component {
   render () {
     return (
       <div>
-
-
-
-        <h4>Cable Binding</h4>
-          <li>.75" gumback - 63478</li>
-          <li>1" gumback - 63479</li>
-          <li>Peg into backplane - </li>
-          <li>Cable Strap nylon - </li>
-          <li>Zip Tie 14" 40# - CT40400CO</li>
-          <li>Zip Tie 8" 18# - CT18200C0</li>
-          <hr />
-        <h4>Tape</h4>
-          <li>Black Electrical - 80610833834</li>
-          <li>Black Rubber - </li>
-          <li>Red Electrical - </li>
-          For marking cables
-          <hr />
-        <h4>Components</h4>
-          <li>.02 ohm resister - </li>
-          <li>300 ohm resister -</li>
-          <li>250 ohm resister -</li>
-          Current to voltage conversion
-          <li>250V 2A fuses (auto) - 0287002</li>
-          <li>Zener Diode - </li>
-          <li>1/16 amp pico fuse - </li>
-          <li></li>
-          <li></li>
-          <hr />
         <table>
-          <tr>
-            <th>Ferrules</th><th>Part Number</th><th>Description</th>
-          </tr>
-            <tr>
-              <td>Red Ferrules</td><td>FSD77-8-DSL2</td><td>18 awg wires</td>
-            </tr>
-            <tr>
-              <td>Blue Ferrules</td><td></td><td></td>
-            </tr>
-            <tr>
-              <td>White Ferrules</td><td>FSD75-8-DSL10</td><td>22-20 AWG wires</td>
-            </tr>
-            <tr>
-              <td>Till Ferrules</td><td></td><td></td>
-            </tr>
-            <tr>
-              <td>Gray Ferrules</td><td>FSD81-12-C</td><td>12AWG Wires</td>
-            </tr>
-          <tr>
-            <th>DSub Connectors</th><th>Part Number</th><th>Description</th>
-          </tr>
-              <tr>
-                <td>DB25 Pin</td><td>1757819-8</td><td></td>
-              </tr>
-              <tr>
-                <td>DB25 Socket</td><td>1757820-8</td><td></td>
-              </tr>
-              <tr>
-                <td>DB25 Backshell</td><td>5748676-3</td><td></td>
-              </tr>
-              <tr>
-                <td>DB37 Pin</td><td>1757819-9</td><td></td>
-              </tr>
-              <tr>
-                <td>DB37 Socket</td><td>1757820-9</td><td></td>
-              </tr>
-              <tr>
-                <td>DB Pin</td><td>205089-1</td><td></td>
-              </tr>
-              <tr>
-                <td>DB Socket</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>DB37 Backshell</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>DB25 Backshell</td><td></td><td></td>
-              </tr>
-            <tr>
-              <th>Ethernet</th><th>Part Number</th><th>Description</th>
-            </tr>
-              <tr>
-              <td>Cat 6 connectors</td><td>100010B</td><td></td>
-              </tr>
-            <tr>
-              <th>Cable Securing</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Tape</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Components</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>MS Connectors</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>D38 Connectors</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>TCs</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Epoxies</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Terminal Blocks</th><th>Part Number</th><th>Description</th>
-            </tr>
-              <tr>
-                <td>ST 1.5 Twin Red</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 1.5 Twin Black</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 1.5 Twin Green</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 1.5 Twin Divider</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 1.5 Quat</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>St 1.5 Quat-U</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>St 1.5 Quat Green</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 1.5 Quat Divider</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>St 2.5 Twin red</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 2.5 Twin Black</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 2.5 twin divider</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 4 Twin Gray</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 4 Twin Red</td><td>3037371</td><td></td>
-              </tr>
-              <tr>
-                <td>ST 4 Twin Black</td><td>3037407</td><td></td>
-              </tr>
-              <tr>
-                <td>ST 4 Twin Divider</td><td></td><td></td>
-              </tr>
-              <tr>
-                <td>ST 6 Gray</td><td></td><td>24-8 AWG Wires</td>
-              </tr>
-              <tr>
-                <td>ST 6 Red</td><td></td><td>24-8 AWG Wires</td>
-              </tr>
-              <tr>
-                <td>ST 6 Black</td><td></td><td>24-8 AWG Wires</td>
-              </tr>
-              <tr>
-                <td>ST 6 Divider</td><td></td><td>ATP-ST 6</td>
-              </tr>
-              <tr>
-                <td>E-UK</td><td>1201442</td><td></td>
-              </tr>
-              <tr>
-                <td></td><td></td><td></td>
-              </tr>
-              <tr>
-                <td></td><td></td><td></td>
-              </tr>
-            <tr>
-              <th>Box components</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Drills/Taps</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Cabling</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Heat Shrink</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Soldering</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>NI Hardware</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>PLC Hardware</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Watlow Hardware</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Telecom Hardware</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Power Distribution</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Labels</th><th>Part Number</th><th>Description</th>
-            </tr>
-            <tr>
-              <th>Seal Tite</th><th>Part Number</th><th>Description</th>
-            </tr>
+          <tr><th>Ferrules</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>Red Ferrules</td><td>FSD77-8-DSL2</td><td>18 awg wires</td></tr>
+            <tr><td>Blue Ferrules</td><td></td><td></td></tr>
+            <tr><td>White Ferrules</td><td>FSD75-8-DSL10</td><td>22-20 AWG wires</td></tr>
+            <tr><td>Till Ferrules</td><td></td><td></td></tr>
+            <tr><td>Gray Ferrules</td><td>FSD81-12-C</td><td>12AWG Wires</td></tr>
+            <tr><td>Yellow Ferrules</td><td></td><td>10AWG Wires</td></tr>
+          <tr><th>DSub Connectors</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>DB9 Pin</td><td></td><td></td></tr>
+            <tr><td>DB9 Socket</td><td></td><td></td></tr>
+            <tr><td>DB9 Backshell</td><td></td><td></td></tr>
+            <tr><td>DB25 Pin</td><td>1757819-8</td><td></td></tr>
+            <tr><td>DB25 Socket</td><td>1757820-8</td><td></td></tr>
+            <tr><td>DB25 Backshell</td><td>5748676-3</td><td></td></tr>
+            <tr><td>DB37 Pin</td><td>1757819-9</td><td></td></tr>
+            <tr><td>DB37 Socket</td><td>1757820-9</td><td></td></tr>
+            <tr><td>DB37 Backshell</td><td></td><td></td></tr>
+            <tr><td>DB Pin</td><td>205089-1</td><td></td></tr>
+            <tr><td>DB Socket</td><td></td><td></td></tr>
+          <tr><th>Ethernet</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>Cat 6 connectors</td><td>100010B</td><td></td></tr>
+          <tr><th>Cable Securing</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>.75" gumback</td><td>63478</td><td></td></tr>
+            <tr><td>1" gumback</td><td>63479</td><td></td></tr>
+            <tr><td>Peg into backplane</td><td></td><td></td></tr>
+            <tr><td>Cable Strap nylon</td><td></td><td></td></tr>
+            <tr><td>Zip Tie 14" 40#</td><td>CT40400CO</td><td></td></tr>
+            <tr><td>Zip Tie 8" 18#</td><td>CT18200C0</td><td></td></tr>
+          <tr><th>Tape</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>Black Electrical</td><td>80610833834</td><td></td></tr>
+            <tr><td>Black Rubber</td><td></td><td></td></tr>
+            <tr><td>Red Electrical</td><td></td><td></td></tr>
+          <tr><th>Components</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>.02 ohm resister</td><td></td><td></td></tr>
+            <tr><td>250 ohm resister</td><td></td><td></td></tr>
+            <tr><td>300 ohm resister</td><td></td><td></td></tr>
+            <tr><td>250V 2A fuses (auto)</td><td>0287002</td><td></td></tr>
+            <tr><td>250V 2A Glass Fuses</td><td></td><td></td></tr>
+            <tr><td>Zener Diode</td><td></td><td></td></tr>
+            <tr><td>Diode</td><td></td><td></td></tr>
+            <tr><td>1/16 amp pico fuse</td><td></td><td></td></tr>
+          <tr><th>MS Connectors</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>RTD Instrument</td><td></td><td></td></tr>
+            <tr><td>RTD/ducer Jumper</td><td></td><td></td></tr>
+            <tr><td>10Pin Load Cell</td><td></td><td></td></tr>
+            <tr><td>6 Pin Load Cell</td><td></td><td></td></tr>
+          <tr><th>D38 Connectors</th><th>Part Number</th><th>Description</th></tr>
+          <tr><th>TCs</th><th>Part Number</th><th>Description</th></tr>
+          <tr><th>Epoxies</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>DP 100</td><td></td><td></td></tr>
+            <tr><td>DP 100 nozzle</td><td></td><td></td></tr>
+            <tr><td>DP Gun</td><td></td><td></td></tr>
+          <tr><th>Terminal Blocks</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>ST 1.5 Twin Red</td><td></td><td></td></tr>
+            <tr><td>ST 1.5 Twin Black</td><td></td><td></td></tr>
+            <tr><td>ST 1.5 Twin Green</td><td></td><td></td></tr>
+            <tr><td>ST 1.5 Twin Divider</td><td></td><td></td></tr>
+            <tr><td>ST 1.5 PE</td><td></td><td></td></tr>
+            <tr><td>ST 1.5 Quat</td><td></td><td></td></tr>
+            <tr><td>St 1.5 Quat-U</td><td></td><td></td></tr>
+            <tr><td>St 1.5 Quat Green</td><td></td><td></td></tr>
+            <tr><td>ST 1.5 Quat Divider</td><td></td><td></td></tr>
+            <tr><td>ST 1.5 Quat PE</td><td></td><td></td></tr>
+            <tr><td>St 2.5 Twin red</td><td></td><td></td></tr>
+            <tr><td>ST 2.5 Twin Black</td><td></td><td></td></tr>
+            <tr><td>ST 2.5 twin divider</td><td></td><td></td></tr>
+            <tr><td>ST 2.5 twin PE</td><td></td><td></td></tr>
+            <tr><td>ST 4 Twin Gray</td><td></td><td></td></tr>
+            <tr><td>ST 4 Twin Red</td><td>3037371</td><td></td></tr>
+            <tr><td>ST 4 Twin Black</td><td>3037407</td><td></td></tr>
+            <tr><td>ST 4 Twin Divider</td><td></td><td></td></tr>
+            <tr><td>ST 4 Twin PE</td><td></td><td></td></tr>
+            <tr><td>ST 6 Gray</td><td></td><td>24-8 AWG Wires</td></tr>
+            <tr><td>ST 6 Red</td><td></td><td>24-8 AWG Wires</td></tr>
+            <tr><td>ST 6 Black</td><td></td><td>24-8 AWG Wires</td></tr>
+            <tr><td>ST 6 Divider</td><td></td><td>ATP-ST 6</td></tr>
+            <tr><td>ST 6 PE</td><td></td><td>ATP-ST 6</td></tr>
+            <tr><td>E-UK</td><td>1201442</td><td></td></tr>
+            <tr><td>Red Marshalling distro blocks</td><td></td><td></td></tr>
+            <tr><td>Black Marshalling distro blocks</td><td></td><td></td></tr>
+            <tr><td>Marshalling block end plate</td><td></td><td></td></tr>
+            <tr><td>Fuse Holder</td><td></td><td></td></tr>
+            <tr><td>Fuse Holder with ground</td><td></td><td></td></tr>
+            <tr><td>Terminal Block Jumper 4</td><td></td><td>for 1.5 Blocks</td></tr>
+            <tr><td>Terminal Block Jumper for 2.5</td><td></td><td></td></tr>
+            <tr><td>Terminal Block Jumper for st-4</td><td></td><td></td></tr>
+            <tr><td>Terminal Block Jumper for st-6</td><td></td><td></td></tr>
+            <tr><td>K-Type Terminal</td><td></td><td></td></tr>
+            <tr><td>E-type Terminal</td><td></td><td></td></tr>
+            <tr><td>2.5 TC shield</td><td></td><td></td></tr>
+          <tr><th>Box components</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>36X24X10 Box</td><td></td><td></td></tr>
+            <tr><td>36X24 Backplane</td><td></td><td></td></tr>
+            <tr><td>Din Rail</td><td></td><td></td></tr>
+            <tr><td>Isolators</td><td></td><td></td></tr>
+            <tr><td>Handles</td><td></td><td></td></tr>
+            <tr><td>Feet</td><td></td><td></td></tr>
+            <tr><td>1" Cable Grip</td><td></td><td></td></tr>
+            <tr><td>1" Jam Nut</td><td></td><td></td></tr>
+            <tr><td>1/2" Cable Grip</td><td></td><td></td></tr>
+            <tr><td>1/2" Jam Nut</td><td></td><td></td></tr>
+          <tr><th>Drills/Taps</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>6-32 Tap</td><td></td><td></td></tr>
+            <tr><td>#35 Drill Bit</td><td></td><td>For 6-32 tap</td></tr>
+            <tr><td>1/4-20 Tap</td><td></td><td></td></tr>
+            <tr><td>8-32 Tap</td><td></td><td></td></tr>
+            <tr><td>1 3/8" Hole Saw</td><td></td><td></td></tr>
+          <tr><th>Cabling</th><th>Part Number</th><th>Description</th></tr>
+          <tr><th>Heat Shrink</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>3/16 shrink</td><td></td><td></td></tr>
+            <tr><td>1/16 shrink</td><td></td><td></td></tr>
+            <tr><td>1" Clear Shrink</td><td></td><td></td></tr>
+          <tr><th>Soldering</th><th>Part Number</th><th>Description</th></tr>
+          <tr><th>NI Hardware</th><th>Part Number</th><th>Description</th></tr>
+          <tr><th>PLC Hardware</th><th>Part Number</th><th>Description</th></tr>
+          <tr><th>Watlow Hardware</th><th>Part Number</th><th>Description</th></tr>
+          <tr><th>Telecom Hardware</th><th>Part Number</th><th>Description</th></tr>
+          <tr><th>Power Distribution</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>Quint Diode</td><td></td><td></td></tr>
+            <tr><td>CT</td><td></td><td></td></tr>
+            <tr><td>Quint Diode</td><td></td><td></td></tr>
+            <tr><td>Breaker Base</td><td></td><td></td></tr>
+            <tr><td>2A Breaker</td><td></td><td></td></tr>
+            <tr><td>5A Breaker</td><td></td><td></td></tr>
+            <tr><td>16A Breaker</td><td></td><td></td></tr>
+            <tr><td>10A Breaker</td><td></td><td></td></tr>
+            <tr><td>19A Breaker</td><td></td><td></td></tr>
+            <tr><td>Glass Cube Base</td><td></td><td></td></tr>
+            <tr><td>Glass Cube</td><td></td><td></td></tr>
+          <tr><th>Labels</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>1/4 Shrink</td><td></td><td></td></tr>
+            <tr><td>3/16 shrink</td><td></td><td></td></tr>
+            <tr><td>5MM shrink</td><td></td><td></td></tr>
+            <tr><td>1/2" Lables</td><td></td><td></td></tr>
+            <tr><td>1/4" Lables</td><td></td><td></td></tr>
+            <tr><td>1" Labels</td><td></td><td></td></tr>
+          <tr><th>Seal Tite</th><th>Part Number</th><th>Description</th></tr>
             <tr><td>Straight Connector</td><td>88073463</td><td></td></tr>
             <tr><td>45 degree Connector</td><td>88073427</td><td></td></tr>
             <tr><td>90 degree connector</td><td>88073428</td><td></td></tr>
-
-          </table>
-
+          <tr><th>Hardware</th><th>Part Number</th><th>Description</th></tr>
+            <tr><td>6-32X.5" screw</td><td></td><td></td></tr>
+            <tr><td>6 Washer</td><td></td><td></td></tr>
+            <tr><td>6 Lock Washer</td><td></td><td></td></tr>
+            <tr><td>Cage Nut</td><td></td><td></td></tr>
+            <tr><td>Cage bolt</td><td></td><td></td></tr>
+          <tr><th>Cabling</th><th>Part Number</th><th>Description</th></tr>
+          <tr><th>Snakeskin</th><th>Part Number</th><th>Description</th></tr>
+        </table>
       </div>
     );
   }
