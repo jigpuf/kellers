4dea75f9ae7c62e2a43b2b2995c3124681e9dfaf
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-08-27 16:18:58 -0500

    added resources for temple study

--- src/components/church/subjects/temple.js
+++ src/components/church/subjects/temple.js
@@ -16,6 +16,7 @@ class Temple extends React.Component {
           <li>D&C</li>
           <li>Abraham</li>
           <li>Moses</li>
+          <li>Genesis</li>
           <li>Leviticus</li>
           <li>Hebrews</li>
         <h4>Talks</h4>
@@ -31,9 +32,20 @@ class Temple extends React.Component {
           <li>Shapes</li>
             Circle, Square, Vesica pesces,
           <li>Plants</li>
-            Fig, Acasia,
+            Fig, Acasia, Almond
         <h4>Words</h4>
           <li>Temple</li>
+          1 Sam 1, 3, 22; 1 Kings 6-8, 14; 2 Kings 11, 12, 16, 18, 22-25;
+          1 Chron 6, 10, 17, 22, 28, 29; 2 Chron 2-6, 23, 26, 27, 29, 35, 36;
+          Ezra 1-7; Nehemiah 12, 13; Isaiah 2, 6, 44, 66; Jeremiah 24, 50, 51;
+          Ezekiel 8, 40-44; Daniel 5; Hosea 8; Joel 3; Amos 8; Jonah 2; Micah 1,
+          4; Habakkuk 2; Haggai 1, 2; Zehariah 1, 2, 4, 6, 8; Malachi 3;
+          Mathew 4, 12, 21, 23, 24, 26, 27; Mark 11-15; Luke 1, 2, 4, 11, 18-24;
+          John 2, 5, 7, 8, 10, 11, 18; Acts 2-5, 7, 17, 19, 21, 22, 24-26;
+          1 Cor 3, 6, 8, 9; 2 Cor 6; Eph 2; 2 Thes 2; Rev 3, 7, 11, 14-16, 21;
+          2 Ne 5, 12, 16; Jacob 1, 2; Mosiah 1, 2, 7, 11, 19; Alma 7, 10, 16,
+          23, 26, 34; Helaman 3, 4, 10; 3 Ne 11, 24; D&C 36, 42, 45, 57-60, 84,
+          88, 93, 97, 101, 109, 110, 124, 127, 128, 133, 137, 138; Fascimile 2
           <li>Holy Order</li>
           <li>New and Everlasting Covenant</li>
           <li>Tokens</li>
@@ -48,13 +60,30 @@ class Temple extends React.Component {
           <li>Creation</li>
           <li>Fall</li>
           <li>Veil</li>
-          <li>FOundation</li>
+          <li>Foundation</li>
           <li>Elijah</li>
           <li>Showbread</li>
           <li>Holy of Holies</li>
           <li>Brazen Sea</li>
           <li>Sacrifice</li>
-          <li></li>
+          <li>Hand</li>
+          <li>Palm</li>
+          <li>finger</li>
+          <li>knuckle</li>
+          <li>Sure Sign</li>
+          <li>Nail</li>
+          <li>Wrist</li>
+          <li>Right</li>
+          <li>Left</li>
+          <li>Shoulder</li>
+          <li>Nail</li>
+          <li>Tabernacle</li>
+          <li>Mountain of the Lord</li>
+          <li>Sinai</li>
+          <li>Priest</li>
+          <li>King</li>
+          <li>Priestess</li>
+          <li>Queen</li>
           <li></li>
           <li></li>
       </div>
