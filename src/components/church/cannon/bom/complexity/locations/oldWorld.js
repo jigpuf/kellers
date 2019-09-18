a99464dea868ee6a37b5460a4502d7aeb9def7e5
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-09-17 23:35:55 -0500

    Update oldWorld.js

--- src/components/church/cannon/bom/complexity/locations/oldWorld.js
+++ src/components/church/cannon/bom/complexity/locations/oldWorld.js
@@ -4,29 +4,96 @@ class OldWorld extends React.Component {
   render () {
     return (
       <div>
-        <li>Jerusalem</li>
-        1Ne 1-5, 7, 10, 11, 16-19, 22; 2Ne 1, 5, 6, 8, 9, 10, 12-15, 17, 18, 20,
-        25, 30; Jacob 1, 2, 7; Enos 1, Omni 1; Mosiah 1, 2, 6, 7, 10, 12, 15,
-        28, 29; Alma 3, 7, 9-11, 18, 21, 22, 28,36, 54; Helaman 5, 7, 8, 16;
-        3 Ne 1, 2, 4, 5, 9, 10, 15-17, 20, 21, 24; 4 Ne 1; Mormon 3; Ether 13;
-        Moroni 10<br />
-        <li>Land of Lehi's Inheritance</li>
-        1 Ne 2, 3, 5, 17<br />
-        <li>Valley of Laman and Lemuel</li>
-        1 Ne 2, 9, 10, 16<br />
-        <li>Shazer</li>
-        1 Ne 16<br />
-        <li>Mouth/Fountain of the Red Sea</li>
-        1 Ne 2<br />
-        <li>Borders near the Red Sea</li>
-        1 Ne 2, 16<br />
-        <li>Nehom</li>
-        1 Ne 16<br />
-        <li>Bountiful</li>
-        1 Ne 17<br />
-        <li>Irreantum</li>
-        1 Ne 17<br />
-        <li>Tower of Babel/Great Tower</li>
+        <h3>The Old World</h3>
+        <p>
+        The First 7 chapters of the Book of Mormon deal with the back and forth
+        between Lehi's family in the wilderness and Jerusalem. Chapters 8-16 all
+        take place in the valley of Lemuel.  In Chapter 16, they recieve
+        instructions to leave the Valley and begin their trek through  Nahom,
+        Shazer, and eventually to Bountiful and Irreantum, where they will leave
+        the old World.
+        </p>
+        <p>
+        The first 7 chapters have the following Locations: Jerusalem, the land
+        of Jerusalem, The Borders near the red sea, The wilderness in the Borders
+        nearer the Red Sea, The land of Lehi's inheritance, the Valey of Lemuel,
+        and the River of Laman that goes to the Fountain of the Red Sea.
+        Also the houses of Lehi, Laban, and Ishmael.
+        </p>
+        <h4>The land of Jerusalem</h4>
+        1 Ne 1-5, 7(600 - 592 BC)<br />
+        <h5>Lehi's House and land of inheritance</h5>
+        1 Ne 1-3<br />
+        <p>
+        The Land of Jerusalem obviously surounds the city of Jerusalem, though
+        it is impossible from the text to tell how far.  Lehi lived "at" Jerusalem
+        all his days which may imply that he lived outside of the city proper.
+        Possibly in a suburb.(1 Ne 1:4)  In the first Year of the reign of Zedekiah, Lehi
+        already had 4 boys and probably some number of daughters. Nephi, the
+        youngest was already old enough to travel alone into the city in chapter
+        3, but young enough to have three older, unmarried brothers.  This would
+        make sense for Nephi and his brothers to be teenaged since marrying as a
+        teen would have been common for the time.  Since Lehi's own house is said
+        to be "at" Jerusalem, some of the elements of the story may feel a bit
+        different in the exchange between Laban and his sons.  It was at Lehi's
+        house where he would have his second vision and his throne theophony.
+        While in vision, he read a heavenly book and prophecied the Babylonian
+        captivity.(1 Ne 1:7-13)  After the vision in his house, Lehi went forth preaching to
+        the Jews, though it is not clear if he went into Jerusalem to preach.
+        It is probably in his house where he has his third vision where he is
+        told to take his family into the wilderness.  The text says he left "His
+        house, and the land of his inheritance... and his precious things".(1 Ne 2:1-4)  So
+        the land of Lehi's inheritance seems like it is in the Land of Jerusalem
+        since he leaves it when he leaves his house where people want to kill
+        him, which is probably near where he was preaching.  From there, he
+        traveled down to the borders near the red sea.  His sons would later
+        come back to this house durring their first trip back to Jerusalem to
+        take all the precious things they left behind to exchange them with
+        Laban for the brass plates.  That will be the last time they see their
+        house in the text.(1 Ne 3:22-23)
+        </p>
+        <h5>Laban's House</h5>
+        1 Ne 3-4<br />
+        <p>
+        Laban's house seems to be in Jerusalem.  It is the first time the story
+        specifically goes into the city itself.  It may have happened before, but
+        the text was not explicit.  While Nephi and his brother's, in chapter 3,
+        first go up to the land of Jerusalem, Laban's house would be in Jerusalem,
+        in the Land of Jerusalem.(1 Ne 3:9-11)  Since the first attempt with Laban fails at
+        obtaining the brass plates, Nephi suggest that they go down to the land
+        of their inheritance to get their possessions.  They then go back up to
+        Laban's house.( 1 Ne 3:16-23)  This suggest that the land of Lehi's inheritance and
+        house are inside of the land of Jerusalem but possibly down in topography.
+        On the second attempt Laban stole their belongings and his servants
+        chased the 4 boys away.  I wonder if Zoram was one of these servants.(1Ne 3:24-27)
+        After this event Laban went out among the Elders of Judah to do whatever
+        they do. The result is he wound up drunk and under the power of Nephi.
+        Nephi had to sneak back into the city this time.  After killing Laban
+        and taking his clothing, Nephi found zoram and got into the treasury of
+        Laban.  From there, he was able to get the brass plates out of Jerusalem
+        and took Zoram with him.(1 Ne 4)
+        </p>
+        <h5>Ishmael's House</h5>
+        1 Ne 7<br />
+        <p>
+        The final trip up to the land of Jerusalem and to Jerusalem would lead to the
+        house of Ishmael.  Here it actually uses the term "we" spoke unto him
+        the words of the Lord, which means Laman and Lemuel may have been on
+        their best behavior.  Ishmael's entire family leaves and this will
+        be the last time anyone from these two families will ever see Jerusalem,
+        the land of Jerusalem, or the lands of their inheritance.(1 Ne 7:1-5)  Though
+        Jerusalem is refered to for the wikidness of it's people and the comming
+        of Christ, it will not enter the timeline of anyone in the Book of
+        Mormon again, with the exception of Christ.  The Baylonians will come.
+        They will destroy the city. They will enslave the people.  After the
+        Jews return from Persia, they will re-establish Jerusalem, rebuild the
+        temple, and crucify Jesus.
+        </p>
+        <h4>Borders, Valleys, Wilderness, Rivers</h4>
+        1 Ne 2-5, 7-17<br />
+        <h4>Nahom, Shazer, Bountiful, Irreantum</h4>
+        1 Ne 16-18<br />
+        <h4>Tower of Babel/Great Tower</h4>
         Omni 1, Mosiah 28, Helaman 6, Ether 1<br />
       </div>
 
