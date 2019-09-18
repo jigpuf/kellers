7ebfefdb4b3c5225519538860bc3e9d772df4526
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-09-17 20:04:29 -0500

    Update oldWorld.js

--- src/components/church/cannon/bom/complexity/locations/oldWorld.js
+++ src/components/church/cannon/bom/complexity/locations/oldWorld.js
@@ -5,16 +5,29 @@ class OldWorld extends React.Component {
     return (
       <div>
         <li>Jerusalem</li>
+        1Ne 1-5, 7, 10, 11, 16-19, 22; 2Ne 1, 5, 6, 8, 9, 10, 12-15, 17, 18, 20,
+        25, 30; Jacob 1, 2, 7; Enos 1, Omni 1; Mosiah 1, 2, 6, 7, 10, 12, 15,
+        28, 29; Alma 3, 7, 9-11, 18, 21, 22, 28,36, 54; Helaman 5, 7, 8, 16;
+        3 Ne 1, 2, 4, 5, 9, 10, 15-17, 20, 21, 24; 4 Ne 1; Mormon 3; Ether 13;
+        Moroni 10<br />
         <li>Land of Lehi's Inheritance</li>
+        1 Ne 2, 3, 5, 17<br />
         <li>Valley of Laman and Lemuel</li>
-        <li>Shazar</li>
-        <li>Red Sea</li>
-        <li>Mouth of the Red Sea</li>
-        <li>Borders of the Red Sea</li>
+        1 Ne 2, 9, 10, 16<br />
+        <li>Shazer</li>
+        1 Ne 16<br />
+        <li>Mouth/Fountain of the Red Sea</li>
+        1 Ne 2<br />
+        <li>Borders near the Red Sea</li>
+        1 Ne 2, 16<br />
         <li>Nehom</li>
+        1 Ne 16<br />
         <li>Bountiful</li>
+        1 Ne 17<br />
         <li>Irreantum</li>
+        1 Ne 17<br />
         <li>Tower of Babel/Great Tower</li>
+        Omni 1, Mosiah 28, Helaman 6, Ether 1<br />
       </div>
 
     );
