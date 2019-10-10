177b3beb9bcbf77817de18979898887c96c4d90e
jigpuf <32025384+jigpuf@users.noreply.github.com>
2019-10-10 03:12:33 -0500

    added tables and lists to html

--- src/components/church/cannon/bom/reading.js
+++ src/components/church/cannon/bom/reading.js
@@ -17,15 +17,29 @@ class Reading extends React.Component {
             </table>
           <h3>Sections</h3>
             <table>
-              <tr><th>Book and Chapters</th><th>Theme</th><th>pages</th></tr>
-              <tr><td>1 Ne 1-7</td><td>Leaving Jerusalem</td><td>1-14</td></tr>
-              <tr><td>1 Ne 8-15</td><td>Visions in the Valley</td><td>14-33</td></tr>
-              <tr><td>1 Ne 16-18</td><td>Travels to the Promised Land</td><td>33-43</td></tr>
-              <tr><td>1 Ne 19-22</td><td>Nephi's sermon in the Land of First Inheritance</td><td>43-53</td></tr>
-              <tr><td>2 Ne 1-4</td><td>Lehi's Last words</td><td>53-65</td></tr>
-              <tr><td></td><td></td><td></td></tr>
-              <tr><td></td><td></td><td></td></tr>
-              <tr><td></td><td></td><td></td></tr>
+              <tr><th>Number</th><th>Book and Chapters</th><th>Theme</th><th>pages</th></tr>
+              <tr><td>1</td><td>1 Ne 1-7</td><td>Leaving Jerusalem</td><td>1-14</td></tr>
+              <tr><td>2</td><td>1 Ne 8-15</td><td>Visions in the Valley</td><td>14-33</td></tr>
+              <tr><td>3</td><td>1 Ne 16-18</td><td>Travels to the Promised Land</td><td>33-43</td></tr>
+              <tr><td>4</td><td>1 Ne 19-22</td><td>Nephi's sermon in the Land of First Inheritance</td><td>43-53</td></tr>
+              <tr><td>5</td><td>2 Ne 1-4</td><td>Lehi's Last words</td><td>53-65</td></tr>
+              <tr><td>6</td><td>2 Ne 5-10</td><td>Separation, Temple, Jacob's Sermon</td><td></td></tr>
+              <tr><td>7</td><td>2 Ne 11-24</td><td>Nephi's Isaiah Fest</td><td></td></tr>
+              <tr><td>8</td><td>2 Ne 25-30</td><td>Nephi's Prophecy</td><td></td></tr>
+              <tr><td>9</td><td>2 Ne 31-33</td><td>The Doctrine of Christ</td><td></td></tr>
+              <tr><td>10</td><td>Jacob 1-4</td><td>Jacob's Sermon</td><td></td></tr>
+              <tr><td>11</td><td>Jacob 5-7</td><td>Allegory of the Olive Tree, Sherem</td><td></td></tr>
+              <tr><td>12</td><td>Enos, Jarom,Omni, WOM</td><td>Descent to wikidness, Loss of Land</td><td></td></tr>
+              <tr><td>13</td><td>Mosiah 1-6</td><td>King Benjamin's Sermon</td><td>146-159</td></tr>
+              <tr><td>14</td><td>Mosiah7-</td><td></td><td></td></tr>
+              <tr><td>15</td><td></td><td></td><td></td></tr>
+              <tr><td>16</td><td></td><td></td><td></td></tr>
+              <tr><td>17</td><td></td><td></td><td></td></tr>
+              <tr><td>18</td><td></td><td></td><td></td></tr>
+              <tr><td>19</td><td></td><td></td><td></td></tr>
+              <tr><td>20</td><td></td><td></td><td></td></tr>
+              <tr><td>21</td><td></td><td></td><td></td></tr>
+              <tr><td>22</td><td></td><td></td><td></td></tr>
             </table>
 
 
--- src/components/programming/html/display.js
+++ src/components/programming/html/display.js
@@ -5,7 +5,46 @@ class Display extends React.Component {
     return (
       <div>
         <h2>Tables</h2>
+          <p>
+            &lt;table&gt;<br />
+                 &lt;tr&gt;&lt;th&gt;Number&lt;/th&gt;&lt;th&gt;Name&lt;/th&gt;&lt;/tr&gt;<br />
+                 &lt;tr&gt;&lt;td&gt;1&lt;/td&gt;&lt;td&gt;Porpois Oriface&lt;/td&gt;&lt;/tr&gt;<br />
+            &lt;/table&gt;<br />
+          </p>
+          Renders:<br />
+          <table>
+            <tr><th>Number</th><th>Name</th></tr>
+            <tr><td>1</td><td>Porpois Oriface</td></tr>
+          </table>
+          <hr />
         <h2>Lists</h2>
+          <div class='twoColumn'>
+          Ordered List:
+          <p>
+            &lt;ol&gt;<br />
+                 &lt;li&gt;2Pac&lt;/li&gt;<br />
+                 &lt;li&gt;Eminem&lt;/li&gt;<br />
+            &lt;/ol&gt;<br />
+          </p>
+          Renders:<br />
+          <ol>
+            <li>2Pac</li>
+            <li>Eminem</li>
+          </ol>
+          Unordered List:
+          <p>
+            &lt;ul&gt;<br />
+                 &lt;li&gt;2Pac&lt;/li&gt;<br />
+                 &lt;li&gt;Eminem&lt;/li&gt;<br />
+            &lt;/ul&gt;<br />
+          </p>
+          Renders:<br />
+          <ul>
+            <li>2Pac</li>
+            <li>Eminem</li>
+          </ul>
+        </div>
+        <hr />
         <h2>Paragraphs</h2>
         <h2>Div</h2>
         <h2>Break</h2>
