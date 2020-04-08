1146eb387b6c3a8e2887fa00d65fa50cd15ab084
jigpuf <32025384+jigpuf@users.noreply.github.com>
2020-04-08 03:28:46 -0500

    Update arrow.js

--- src/components/programming/js/functions/arrow.js
+++ src/components/programming/js/functions/arrow.js
@@ -5,6 +5,15 @@ class Arrow extends React.Component {
     return (
       <div>
         <h2>Arrow Functions</h2>
+          <table>
+            <tr><th>Function Type</th><th>code</th></tr>
+            <tr><td>Standard Function</td><td>Const functionName = function(parameter)&#123;<br />
+            code;<br />
+            return(code);<br />
+          };<br /></td></tr>
+            <tr><td>Arrow Function</td><td>const functionName = (parameter) =&#62;&#123;<br />code;<br /> return(code);<br />&#125;;</td></tr>
+            <tr><td>Single Line Arrow Function<br /> Must have no code other<br /> than return statement</td><td>const functionName = (parameter) =&#62; returnCode;</td></tr>
+          </table>
       </div>
     );
   }
