820e3adc57960e30e1a447b1474d968836ffd618
jigpuf <32025384+jigpuf@users.noreply.github.com>
2021-01-15 21:49:57 -0600

    Update stockScreener.js

--- src/components/apps/stockScreener.js
+++ src/components/apps/stockScreener.js
@@ -1,27 +1,44 @@
 import React, {useState} from 'react';
 
 const StockScreener = () => {
-  const [spread,setSpread] = useState({high:100, low:0});
-  const [startLow,setStartLow] = useState(null);
+  const [spread,setSpread] = useState({high:100, low:0, startLow:0});
+  const [symbol,setSymbol]= useState(null);
+  
+  const [saveSymbol, setSaveSymbol]= useState(null);
+  const [saveHigh, setSaveHigh]= useState(null);
+  const [saveLow, setSaveLow]= useState(null);
+  const [saveStartLow, setSaveStartLow]= useState(null);
 
 
+
+  const [saveSpread,setSaveSpread] = useState({high:0, low:0, startLow:0});
+
+
+
+  const changeSymbol = (event) => {
+    const value = event.target.value;
+    setSymbol(value);
+  }
 
   const changeSpread = (event) => {
     const name = event.target.name;
     const value = parseInt(event.target.value,10);
-    setSpread({...spread, [name]:value});
+    setSpread({...spread,[name]:value});
   }
-  const changeStartLow = (event) => {
-    const name = event.target.name;
-    const value = parseInt(event.target.value,10);
-    setStartLow(value);
+  const saver = () =>{
+    setSaveSymbol(symbol);
+    setSaveHigh(spread.high);
+    setSaveLow(spread.low);
+    setSaveStartLow(spread.startLow);
   }
+
+
     return (
       <div>
       Symbol:<input
-        name="symbol"
-        type="text"
-        value=""/>
+        type='text'
+        value={symbol}
+        onChange={changeSymbol}/>
         <br />
       High:<input
         name="high"
@@ -38,14 +55,44 @@ const StockScreener = () => {
       Start Low:<input
         name="startLow"
         type="number"
-        value={startLow}
-        onChange={changeStartLow}/>
+        value={spread.startLow}
+        onChange={changeSpread}/>
         <br />
-      Spread:{spread.high-spread.low}<br />
-      Spread Percentage&darr;:{((spread.high-spread.low)/spread.high)*100}%<br />
-      Percent Change&uarr;:{((spread.low-startLow)/startLow)*100}%<br />
-      </div>
+        <table>
+          <tr>
+            <th>Symbol</th>
+            <th>Spread</th>
+            <th>Spread%&darr;</th>
+            <th>Percent Change&uarr;</th>
+          </tr>
+          <tr>
+            <td>{symbol}</td>
+            <td>{(spread.high-spread.low)}</td>
+            <td>{((spread.high-spread.low)/spread.high)*100}%</td>
+            <td>{((spread.low-spread.startLow)/spread.startLow)*100}%</td>
+          </tr>
+        </table>
+        <br />
+        <button onClick={saver}>save</button>
+        <br />
+        <br />
+        <table>
+          <tr>
+            <th>Symbol</th>
+            <th>Spread</th>
+            <th>Spread%&darr;</th>
+            <th>Percent Change&uarr;</th>
+          </tr>
+          <tr>
+            <td>{saveSymbol}</td>
+            <td>{(saveHigh-saveLow)}</td>
+            <td>{((saveHigh-saveLow)/saveHigh)*100}%</td>
+            <td>{((saveLow-saveStartLow)/saveStartLow)*100}%</td>
+          </tr>
 
+        </table>
+
+      </div>
     );
 }
 export default StockScreener;
